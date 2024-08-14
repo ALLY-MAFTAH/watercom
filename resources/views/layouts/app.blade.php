7ae3e7e671967ca6d5511d19cb1324d42387261e
Ally Waziri Maftah <maflampizzle69@gmail.com>
2024-08-15 01:35:14 +0300

    fex: Corrected date compare in expenses

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -254,7 +254,7 @@
     </main>
 
     </div>
-    {{-- <x:notify-messages /> --}}
+    <x:notify-messages />
     @yield('scripts')
     @notifyJs
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
